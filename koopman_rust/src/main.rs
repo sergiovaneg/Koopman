f35a5de9a861efecd3f20e80e5d0e0e1e49b9764
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-03-31 20:43:34 +0200

    Added default to console input.

--- koopman_rust/src/main.rs
+++ koopman_rust/src/main.rs
@@ -1,10 +1,16 @@
 mod vdp_sim;
 
+use std::env;
+
 fn main() {
+    let args    : Vec<String>   = env::args().collect();
+    let dir     : &str          =
+        if args.len()>=2    {&args[1]}
+        else                {"/home/sergiovaneg/Documents/Thesis/Rust_Data/"};
+
     let period  = 20.;
     let ts      = 0.01;
     let n       = 100;
-    let dir     = "/home/sergiovaneg/Documents/Thesis/Rust_Data/";
 
-    vdp_sim::gen_data(n, period, ts, &dir);
+    vdp_sim::gen_data(n, period, ts, dir);
 }
