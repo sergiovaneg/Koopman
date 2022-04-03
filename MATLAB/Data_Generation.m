b7a3fc7a675d2c8339e5ecb592538db059f6892e
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-04-03 16:16:27 +0200

    Updated to use a Noisy Steady Input as training data.

--- MATLAB/Data_Generation.m
+++ MATLAB/Data_Generation.m
@@ -3,7 +3,7 @@
 close all;
 clearvars;
 clc;
-Data_Source = "~/Documents/Thesis/VanDerPol_Unsteady_Input/";
+Data_Source = "~/Documents/Thesis/VanDerPol_Noisy_Unsteady_Input/";
 
 %% Oscillator
 
@@ -13,6 +13,7 @@ N_Sim = 100;
 
 u_t = 0:ts:T;
 sigma = 1; % 0 for constant input
+eta = 0.1; % 0 for no signal noise
 mu = 0;
 
 system("mkdir -p "+Data_Source);
@@ -20,11 +21,11 @@ delete(Data_Source+"Data_*.mat");
 
 parfor f=1:N_Sim
     mu = randn(1); % 0 for zero-mean noise
-    u = sigma^2*randn(size(u_t)) + mu;
+    u = sigma*randn(size(u_t)) + mu;
     z0 = 2*rand(2,1) - 1;
     [~,z] = ode113(@(t,z) VanDerPol(t,z,u_t,u), u_t, z0);
     
-    z = z';
+    z = (z + eta*randn(size(z)))';
     L = length(z)-1;
     parsave(sprintf(Data_Source+'Data_%i.mat',f),L,z,u,T,ts);
 end
--- MATLAB/Koopman_Evaluation.m
+++ MATLAB/Koopman_Evaluation.m
@@ -8,9 +8,10 @@ Data_Source = "~/Documents/Thesis/VanDerPol_Unsteady_Input/";
 
 %% Operator retrieval
 
-P = [5;10;12;13];
-M = [10;20;50;100;150];
-load(Data_Source+'Operator_P_5.mat',"ts");
+P = 11;
+M = 100;
+Alpha = (4:-1:3)';
+load(Data_Source+'Polynomic/Operator_alpha_none.mat',"ts");
 
 %% Trajectory generation
 
@@ -31,11 +32,23 @@ markerDecay = 0.003;
 
 figure(1);
 scatter(z(1,:),z(2,:),36*exp(-markerDecay*(0:L-1)));
+title("Polynomic Observables");
 hold on;
-for i=1:length(P)
-    load(sprintf(Data_Source+'Operator_P_%i.mat',P(i)));
+
+load(Data_Source+'Polynomic/Operator_alpha_none.mat');
+g_p = zeros(size(B,1),L);
+g_p(:,1) = Poly_Obs(z(:,1),P);
+for j=1:L-1
+    g_p(:,j+1) = A*g_p(:,j) + B*u(:,j);
+end
+z_p = C*g_p;
+scatter(z_p(1,:),z_p(2,:),36*exp(-markerDecay*(0:L-1)));
+
+for i=1:length(Alpha)
+    load(sprintf(Data_Source ...
+        +'Polynomic/Operator_alpha_%i.mat',Alpha(i)));
     g_p = zeros(size(B,1),L);
-    g_p(:,1) = Poly_Obs(z(:,1),P(i));
+    g_p(:,1) = Poly_Obs(z(:,1),P);
     
     for j=1:L-1
         g_p(:,j+1) = A*g_p(:,j) + B*u(:,j);
@@ -44,15 +57,29 @@ for i=1:length(P)
     z_p = C*g_p;
     scatter(z_p(1,:),z_p(2,:),36*exp(-markerDecay*(0:L-1)));
 end
-legend(["Reference";int2str(P)]);
+legend(["Reference"; ...
+    "Alpha=0"; ...
+    repmat("Alpha = 10^-",length(Alpha),1)+int2str(Alpha)]);
 hold off;
+
 
 figure(2);
 scatter(z(1,:),z(2,:),36*exp(-markerDecay*(0:L-1)));
-% plot(z(2,:));
+title("Radial Observables");
 hold on;
-for i=1:length(M)
-    load(sprintf(Data_Source+'Operator_M_%i.mat',M(i)));
+
+load(Data_Source+'Radial/Operator_alpha_none.mat');
+g_p = zeros(size(B,1),L);
+g_p(:,1) = Spline_Radial_Obs(z(:,1),X0);
+for j=1:L-1
+    g_p(:,j+1) = A*g_p(:,j) + B*u(:,j);
+end
+z_p = C*g_p;
+scatter(z_p(1,:),z_p(2,:),36*exp(-markerDecay*(0:L-1)));
+
+for i=1:length(Alpha)
+    load(sprintf(Data_Source ...
+        +'Radial/Operator_alpha_%i.mat',Alpha(i)));
     g_p = zeros(size(B,1),L);
     g_p(:,1) = Spline_Radial_Obs(z(:,1),X0);
     
@@ -62,7 +89,8 @@ for i=1:length(M)
     
     z_p = C*g_p;
     scatter(z_p(1,:),z_p(2,:),36*exp(-markerDecay*(0:L-1)));
-%     plot(z_p(2,:));
 end
-legend(["Reference";int2str(M)]);
+legend(["Reference"; ...
+    "Alpha=0"; ...
+    repmat("Alpha = 10^-",length(Alpha),1)+int2str(Alpha)]);
 hold off;
