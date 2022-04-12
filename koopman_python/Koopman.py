4a2ca52b0e5d263a12f100e88cf1a38f0b828927
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-04-12 17:37:13 +0200

    Started experimenting with regularized least squares.

--- koopman_python/Koopman.py
+++ koopman_python/Koopman.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
 
 from cmath import inf
+import numpy as np
 from docplex.mp.model import Model
 
 
 mdl = Model('my_test');
+aux = mdl.infinity;
+coeffs = [1,2,3];
+x = mdl.continuous_var_list(range(3),0, None, 'x');
 
-x = mdl.continuous_var(0, None, 'x');
-y = mdl.integer_var(0, None, 'y');
-z = mdl.binary_var('z');
-
-mdl.add(x**2 >= 1);
-mdl.add(y**2 >= 2);
-mdl.add(z**2+y**2 <= 5);
-
-obj_fun = x**2 + y**2 + z**2;
-mdl.set_objective('min', obj_fun);
-
-mdl.print_information();
-mdl.solve();
-mdl.print_solution();
-
-mdl.add(z**2 + y**2 <= x**2);
+mdl.set_objective('min',mdl.sumsq((x[i]-coeffs[i]) for i in range(3)) + 0.001*mdl.sumsq(x));
 mdl.solve();
 mdl.print_solution();
