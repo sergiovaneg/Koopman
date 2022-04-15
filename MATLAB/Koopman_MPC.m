c069d2fddeb8bdc5065687bdf147fb952eeb2b18
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-04-15 17:38:42 +0200

    Changed the amount of controlled variables. Needed evaluation of MPC against real oscillator.

--- MATLAB/Koopman_MPC.m
+++ MATLAB/Koopman_MPC.m
@@ -7,17 +7,19 @@ Data_Source = "~/Documents/Thesis/VanDerPol_Noisy_Unsteady_Input/Radial/";
 
 %% Operator retrieval
 
-alpha = 2;
-load(sprintf(Data_Source+'Operator_alpha_%i.mat',alpha));
+alpha = 3;
+load(sprintf(Data_Source+'Operator_M_30_alpha_%i.mat',alpha));
 
 VanDerPol = ss(A,B,C,D,ts);
-step(VanDerPol);
+% step(VanDerPol);
+VanDerPol = setmpcsignals(VanDerPol,'MV',1,'MO',2,'UO',1);
 
 mpcobj = mpc(VanDerPol,ts);
-mpcobj.PredictionHorizon = 20;
+mpcobj.PredictionHorizon = 25;
 % review(mpcobj);
+mpcobj.MV.Min = -10;
+mpcobj.MV.Max = 10;
 
-r = [-1 1;
-    0 0];
-T = 1e4;
+r = [0 1];
+T = 1e5;
 sim(mpcobj,T,r);
