39ad1ae74b4e17e7a40221ce5f552428a1c47545
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-04-17 12:52:14 +0200

    Modified implementation to avoid ln(0) using the technique from Wikipedia.

--- MATLAB/Spline_Radial_Obs.m
+++ MATLAB/Spline_Radial_Obs.m
@@ -8,8 +8,7 @@ function [g,n] = Spline_Radial_Obs(z,X0)
 
     for i=1:N
         x0 = X0(:,i);
-        g(i+n,:) = (vecnorm(z-x0)+sqrt(eps)).^2 ...
-                    .* log(eps+vecnorm(z-x0));
+        g(i+n,:) = vecnorm(z-x0).*log(vecnorm(z-x0).^vecnorm(z-x0));
     end
 
     n=n+N;
