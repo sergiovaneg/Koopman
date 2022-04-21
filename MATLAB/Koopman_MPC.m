c35740f8224350ab83bb3d91e35e6b9de47da433
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-04-21 16:37:41 +0200

    Now applying MPC over observable Space.

--- MATLAB/Koopman_MPC.m
+++ MATLAB/Koopman_MPC.m
@@ -10,12 +10,13 @@ Data_Source = "~/Documents/Thesis/VanDerPol_Noisy_Unsteady_Input/Radial/";
 alpha = 2;
 load(sprintf(Data_Source+'Operator_M_30_alpha_%i.mat',alpha));
 
-VanDerPol = ss(A,B,C,D,ts);
+VanDerPol = ss(A,B,eye(size(A)),zeros(size(B)),ts);
 % step(VanDerPol);
-VanDerPol = setmpcsignals(VanDerPol,'MV',1,'MO',2,'UO',1);
+% VanDerPol = setmpcsignals(VanDerPol,'MO',2,'UO',[1,3:length(A)]);
 
-mpcobj = mpc(VanDerPol,ts);
-mpcobj.PredictionHorizon = 10;
+mpcobj = mpc(VanDerPol,ts,10);
+ovw = zeros(size(A,1),1); ovw(2) = 1;
+mpcobj.Weights.OutputVariables = ovw;
 % review(mpcobj);
 % mpcobj.MV.Min = -10;
 % mpcobj.MV.Max = 10;
