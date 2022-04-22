1f44ffbb306cbc256491a4effb8ee68fed3b838b
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-04-23 01:01:36 +0200

    Finished setting up Koopman Linear MPC for VDP.

--- MATLAB/linear_mpc/Koopman_MPC.m
+++ MATLAB/linear_mpc/Koopman_MPC.m
@@ -3,11 +3,11 @@
 close all;
 clearvars;
 clc;
-Data_Source = "~/Documents/Thesis/VanDerPol_Noisy_Unsteady_Input/Radial/";
+Data_Source = "~/Documents/Thesis/VanDerPol_Clean_Unsteady_Input/Radial/";
 
 %% Operator retrieval
 
-alpha = 2;
+alpha = 3;
 load(sprintf(Data_Source+'Operator_M_30_alpha_%i.mat',alpha));
 
 VanDerPol = ss(A,B,eye(size(A)),zeros(size(B)),ts);
@@ -15,12 +15,16 @@ VanDerPol = ss(A,B,eye(size(A)),zeros(size(B)),ts);
 % VanDerPol = setmpcsignals(VanDerPol,'MO',2,'UO',[1,3:length(A)]);
 
 mpcobj = mpc(VanDerPol,ts,10);
-ovw = zeros(size(A,1),1); ovw(2) = 1;
+ovw = zeros(1,size(A,1)); ovw(2) = 1;
 mpcobj.Weights.OutputVariables = ovw;
+
+mpcobj.PredictionHorizon = 25;
+mpcobj.ControlHorizon = 10;
+
 % review(mpcobj);
 % mpcobj.MV.Min = -10;
 % mpcobj.MV.Max = 10;
 
-% r = [0 1];
+% r = [1 0 zeros(1,30)];
 % T = 1e5;
 % sim(mpcobj,T,r);
