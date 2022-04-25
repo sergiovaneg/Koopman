3a69bae15337ba6806167df072288cba0c4d23c5
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-04-25 22:52:17 +0200

    Last version used for presentation.

--- MATLAB/nonlinear_mpc/VanDerPol_MPC_Evaluation.m
+++ MATLAB/nonlinear_mpc/VanDerPol_MPC_Evaluation.m
@@ -12,14 +12,13 @@ data_source = "~/Documents/Thesis/Nonlinear_MPC_VDP/";
 load(data_source+"vdp_mpc_balanced.mat");
 
 % load Koopman
-M = 20;
-load(sprintf(data_source+'koopman_balanced_differential_M_%i.mat',M));
+load(data_source+'koopman_first_differential_M_26.mat');
 
 %% Set simulation parameters
 
 Tf = 20;
 load(sprintf( ...
-    "~/Documents/Thesis/VanDerPol_Clean_Unsteady_Input/Data_%i.mat",93));
+    "~/Documents/Thesis/VanDerPol_Clean_Unsteady_Input/Data_%i.mat",75));
 R1 = array2timetable(z(1,:)',"TimeStep",seconds(Ts));
 R2 = array2timetable(z(2,:)',"TimeStep",seconds(Ts));
 
