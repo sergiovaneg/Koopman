ce848c326cd848068c91f72040fcebd01c6bd288
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-04-25 20:16:50 +0200

    Added direct/differential switch + dataset range selection.

--- MATLAB/nonlinear_mpc/VanDerPol_MPC_Koopman.m
+++ MATLAB/nonlinear_mpc/VanDerPol_MPC_Koopman.m
@@ -4,26 +4,29 @@ close all;
 clearvars;
 clc;
 addpath("~/Koopman/MATLAB/");
+rng(0,'threefry');
 data_source = "~/Documents/Thesis/Nonlinear_MPC_VDP/";
 
 %% First step - System observation
 
-load(data_source + "data_noisy_balanced.mat");
+load(data_source + "data_noisy_second.mat");
+L = 2e5;
 
-M = 20;
+M = 18;
 n_u = size(U,1);
 X0 = 2*rand(size(Z,1),M)-1;
 
 [G,~] = Spline_Radial_Obs(Z,X0);
-Px = G(:,1:end-1);
-Py = G(:,2:end);
-U = U(1:n_u/2,1:end-1) - U(n_u/2+1:end,1:end-1);
+Px = G(:,1:L);
+Py = G(:,2:L+1);
+% U = U(1:n_u/2,1:L) - U(n_u/2+1:end,1:L);
+U = U(:,1:L);
 
 %% Second step - Operator calculation
 
 tic
 
-alpha = 0.;
+alpha = 1e-3;
 [A,B] = Koopman(Px,Py,U,alpha);
 
 % Original states recovered by convention
@@ -34,7 +37,7 @@ C(1:size(Z,1),1:size(Z,1)) = eye(size(Z,1));
 % Recovery of original states independent from input
 D = zeros(size(Z,1),size(U,1));
 
-save(sprintf(data_source+'koopman_balanced_differential_M_%i.mat',M), ...
+save(sprintf(data_source+'koopman_second_direct_M_%i.mat',M), ...
     "A","B","C","D","Ts","X0");
 
 toc
