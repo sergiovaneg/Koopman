c92f135a2f8bf636191887ecfc33901bb3263259
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-05-01 00:57:54 +0200

    Finished setting up Kalman-Koopman; delayed version still unstable and sub-optimal trajectory tracking.

--- MATLAB/nonlinear_mpc/VanDerPol_MPC_Evaluation.m
+++ MATLAB/nonlinear_mpc/VanDerPol_MPC_Evaluation.m
@@ -12,18 +12,20 @@ data_source = "~/Documents/Thesis/Nonlinear_MPC_VDP/";
 load(data_source+"vdp_mpc_definitive.mat");
 
 % load Koopman
-load(data_source+'koopman_hybrid_delayed_M_25.mat');
+load(data_source+'koopman_kalman_M_25.mat');
 
 %% Set simulation parameters
 
 Tf = 20;
+eta = 0.001;
+
 load(sprintf( ...
     "~/Documents/Thesis/VanDerPol_Clean_Unsteady_Input/Data_%i.mat",70));
 R1 = array2timetable(z(1,:)',"TimeStep",seconds(Ts));
 R2 = array2timetable(z(2,:)',"TimeStep",seconds(Ts));
 
 createParameterBus(nlobj, ...
-    ['Simulink_VDP_comparison' '/vdpMPC'], ...
+    ['Simulink_VDP_KK' '/vdpMPC'], ...
     'Ts_Bus',{Ts});
 
 y_0 = 0;
--- MATLAB/nonlinear_mpc/VanDerPol_MPC_Koopman.m
+++ MATLAB/nonlinear_mpc/VanDerPol_MPC_Koopman.m
@@ -9,22 +9,37 @@ data_source = "~/Documents/Thesis/Nonlinear_MPC_VDP/";
 
 %% First step - System observation
 
-load(data_source + "data_noisy_definitive.mat");
-L = 2e5;
+load(data_source + "data_kalman_definitive.mat");
+L = 1e5;
 
-M = 25;
+M = 50;
 n_u = size(U,1);
 X0 = 2*rand(size(Z,1),M)-1;
-
 [G,~] = Spline_Radial_Obs(Z,X0);
-Px = G(:,1:L);
-Py = G(:,2:L+1);
-U = [U(:,1:L);
-    U(1:n_u/2,1:L) - U(n_u/2+1:end,1:L)];
+
+% Full-State
+% Px = G(:,1:L);
+% Py = G(:,2:L+1);
+% U = [U(:,1:L);
+%     U(1:n_u/2,1:L) - U(n_u/2+1:end,1:L)];
+
+% Full-State + Delay
 % Px = [G(:,3:L);G(:,2:L-1);G(:,1:L-2)];
 % Py = G(:,4:L+1);
 % U = [U(:,3:L);
 %     U(1:n_u/2,3:L) - U(n_u/2+1:end,3:L)];
+
+% Kalman
+% Px = G(:,1:L);
+% Py = G(:,2:L+1);
+% U = [U(:,1:L);
+%     U(1,1:L) - U(3,1:L)];
+
+% Kalman + Delay
+Px = [G(:,3:L);G(:,2:L-1);G(:,1:L-2)];
+Py = G(:,4:L+1);
+U = [U(:,3:L);
+    U(1,3:L) - U(3,3:L)];
 
 %% Second step - Operator calculation
 
@@ -41,7 +56,7 @@ C(1:size(Z,1),1:size(Z,1)) = eye(size(Z,1));
 % Recovery of original states independent from input
 D = zeros(size(Z,1),size(U,1));
 
-save(sprintf(data_source+'koopman_hybrid_M_%i.mat',M), ...
+save(sprintf(data_source+'koopman_kalman_delayed_M_%i.mat',M), ...
     "A","B","C","D","Ts","X0");
 
 toc
