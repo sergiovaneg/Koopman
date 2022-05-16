21bee99204c434b96600f7439eb5e4a085ce3950
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-05-16 22:44:53 +0200

    Reduced the multiplication by the pseudo-inverse by a single step.

--- MATLAB/Koopman.m
+++ MATLAB/Koopman.m
@@ -3,22 +3,20 @@ function [A,B] = Koopman(X,Y,U,alpha,nx)
     K = size(X,2);
 
     G = [X;U]*[X;U]'/K;
-    G_pinv = pinv(G);
     V = Y(1:nx,:)*[X;U]'/K;
+    M0 = V*pinv(G);
     
     A = zeros(nx,size(X,1));
     B = zeros(nx,size(U,1));
 
     for i=1:nx
-        m0 = V(i,:)*G_pinv;
-        
         if alpha>0.0
             cvx_begin
                 variable m(1,size(X,1)+size(U,1))
                 minimize (norm((m*[X;U]-Y(i,:)))/K + alpha*norm(m)/length(m))
             cvx_end
         elseif alpha==0.0
-            m = m0;
+            m = M0(i,:);
         end
 
         A(i,:) = m(1:size(X,1));
