cf56beec99157f9c8fef63c26085622091dd80cc
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-07-17 19:57:41 +0200

    Fixed polynomial observer.

--- MATLAB/Poly_Obs.m
+++ MATLAB/Poly_Obs.m
@@ -1,16 +1,16 @@
-function [g,n] = Poly_Obs(z,P)
+function [g,n,exponents] = Poly_Obs(z,P)
     %POLYOBS Summary of this function goes here
     %   Detailed explanation goes here
     Nx = size(z,1);
     K = size(z,2);
-    n = factorial(Nx+P)/(factorial(Nx)*factorial(P));
+    n = factorial(Nx+P)/(factorial(Nx)*factorial(P))-1;
     g = ones(n,K);
     
-    exponents = zeros(n,Nx);
+    exponents = [eye(Nx);
+                zeros(n-Nx,Nx)];
     current = zeros(1,Nx);
-
-    [exponents,~] = Recursive_Monomial(1,1,exponents,current,P);
-
+    
+    [exponents,~] = Recursive_Monomial(Nx+1,1,exponents,current,P);
     for i=1:n
         for j=1:Nx
             g(i,:) = g(i,:).*(z(j,:).^exponents(i,j));
--- MATLAB/private/Recursive_Monomial.m
+++ MATLAB/private/Recursive_Monomial.m
@@ -1,8 +1,10 @@
 function [exponents,i] = Recursive_Monomial(i,j,exponents,current,P)
     while sum(current) <= P
-        if j==size(exponents,2) && sum(current)>0
-            exponents(i,:) = current;
-            i = i+1;
+        if j==size(exponents,2)
+            if sum(current)>1
+                exponents(i,:) = current;
+                i = i+1;
+            end
         else
             [exponents,i] = Recursive_Monomial(i,j+1,exponents,current,P);
         end
