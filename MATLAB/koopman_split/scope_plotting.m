c9cac09a41f0305ec19506c134d9df8ca7e52317
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2022-09-22 21:25:29 +0200

    Fixed dataset and figure generation.

--- MATLAB/koopman_split/scope_plotting.m
+++ MATLAB/koopman_split/scope_plotting.m
@@ -23,73 +23,73 @@ error = cell(no_exp*(1+4*no_ng),6);
 
 error{1,1} = "step"; error{1,2} = "EKF_nlMPC"; error{1,3} = "";
 error{1,4} = 0;
-error{1,5} = get_NRMSE(results.step.EKF_nlMPC.NRMSE,1);
-error{1,6} = get_NRMSE(results.step.EKF_nlMPC.NRMSE,2);
+error{1,5} = get_NRMSE(results.step.EKF_nlMPC.RMSE,1);
+error{1,6} = get_NRMSE(results.step.EKF_nlMPC.RMSE,2);
 for i=1:no_ng
     error{4*i-2,1} = "step"; error{4*i-2,2} = "K_nlMPC";
     error{4*i-2,3} = "radial";
     error{4*i-2,4} = results.step.K_nlMPC.radial{i}.n_g;
-    error{4*i-2,5} = get_NRMSE(results.step.K_nlMPC.radial{i}.NRMSE,1);
-    error{4*i-2,6} = get_NRMSE(results.step.K_nlMPC.radial{i}.NRMSE,2);
+    error{4*i-2,5} = get_NRMSE(results.step.K_nlMPC.radial{i}.RMSE,1);
+    error{4*i-2,6} = get_NRMSE(results.step.K_nlMPC.radial{i}.RMSE,2);
 
     error{4*i-1,1} = "step"; error{4*i-1,2} = "K_nlMPC";
     error{4*i-1,3} = "covradial";
     error{4*i-1,4} = results.step.K_nlMPC.covradial{i}.n_g;
-    error{4*i-1,5} = get_NRMSE(results.step.K_nlMPC.covradial{i}.NRMSE,1);
-    error{4*i-1,6} = get_NRMSE(results.step.K_nlMPC.covradial{i}.NRMSE,2);
+    error{4*i-1,5} = get_NRMSE(results.step.K_nlMPC.covradial{i}.RMSE,1);
+    error{4*i-1,6} = get_NRMSE(results.step.K_nlMPC.covradial{i}.RMSE,2);
 
     error{4*i,1} = "step"; error{4*i,2} = "K_lMPC";
     error{4*i,3} = "radial";
     error{4*i,4} = results.step.K_lMPC.radial{i}.n_g;
-    error{4*i,5} = get_NRMSE(results.step.K_lMPC.radial{i}.NRMSE,1);
-    error{4*i,6} = get_NRMSE(results.step.K_lMPC.radial{i}.NRMSE,2);
+    error{4*i,5} = get_NRMSE(results.step.K_lMPC.radial{i}.RMSE,1);
+    error{4*i,6} = get_NRMSE(results.step.K_lMPC.radial{i}.RMSE,2);
 
     error{4*i+1,1} = "step"; error{4*i+1,2} = "K_lMPC";
     error{4*i+1,3} = "covradial";
     error{4*i+1,4} = results.step.K_lMPC.covradial{i}.n_g;
-    error{4*i+1,5} = get_NRMSE(results.step.K_lMPC.covradial{i}.NRMSE,1);
-    error{4*i+1,6} = get_NRMSE(results.step.K_lMPC.covradial{i}.NRMSE,2);
+    error{4*i+1,5} = get_NRMSE(results.step.K_lMPC.covradial{i}.RMSE,1);
+    error{4*i+1,6} = get_NRMSE(results.step.K_lMPC.covradial{i}.RMSE,2);
 end
 idx = 4*no_ng+1;
 
 error{idx+1,1} = "osc"; error{idx+1,2} = "EKF_nlMPC"; error{idx+1,3} = "";
 error{idx+1,4} = 0;
-error{idx+1,5} = get_NRMSE(results.osc.EKF_nlMPC.NRMSE,1);
-error{idx+1,6} = get_NRMSE(results.osc.EKF_nlMPC.NRMSE,2);
+error{idx+1,5} = get_NRMSE(results.osc.EKF_nlMPC.RMSE,1);
+error{idx+1,6} = get_NRMSE(results.osc.EKF_nlMPC.RMSE,2);
 for j=1:no_ng
     error{idx+4*j-2,1} = "osc"; error{idx+4*j-2,2} = "K_nlMPC";
     error{idx+4*j-2,3} = "radial";
     error{idx+4*j-2,4} = results.osc.K_nlMPC.radial{j}.n_g;
-    error{idx+4*j-2,5} = get_NRMSE(results.osc.K_nlMPC.radial{j}.NRMSE,1);
-    error{idx+4*j-2,6} = get_NRMSE(results.osc.K_nlMPC.radial{j}.NRMSE,2);
+    error{idx+4*j-2,5} = get_NRMSE(results.osc.K_nlMPC.radial{j}.RMSE,1);
+    error{idx+4*j-2,6} = get_NRMSE(results.osc.K_nlMPC.radial{j}.RMSE,2);
 
     error{idx+4*j-1,1} = "osc"; error{idx+4*j-1,2} = "K_nlMPC";
     error{idx+4*j-1,3} = "covradial";
     error{idx+4*j-1,4} = results.osc.K_nlMPC.covradial{j}.n_g;
-    error{idx+4*j-1,5} = get_NRMSE(results.osc.K_nlMPC.covradial{j}.NRMSE,1);
-    error{idx+4*j-1,6} = get_NRMSE(results.osc.K_nlMPC.covradial{j}.NRMSE,2);
+    error{idx+4*j-1,5} = get_NRMSE(results.osc.K_nlMPC.covradial{j}.RMSE,1);
+    error{idx+4*j-1,6} = get_NRMSE(results.osc.K_nlMPC.covradial{j}.RMSE,2);
 
     error{idx+4*j,1} = "osc"; error{idx+4*j,2} = "K_lMPC";
     error{idx+4*j,3} = "radial";
     error{idx+4*j,4} = results.osc.K_lMPC.radial{j}.n_g;
-    error{idx+4*j,5} = get_NRMSE(results.osc.K_lMPC.radial{j}.NRMSE,1);
-    error{idx+4*j,6} = get_NRMSE(results.osc.K_lMPC.radial{j}.NRMSE,2);
+    error{idx+4*j,5} = get_NRMSE(results.osc.K_lMPC.radial{j}.RMSE,1);
+    error{idx+4*j,6} = get_NRMSE(results.osc.K_lMPC.radial{j}.RMSE,2);
 
     error{idx+4*j+1,1} = "osc"; error{idx+4*j+1,2} = "K_lMPC";
     error{idx+4*j+1,3} = "covradial";
     error{idx+4*j+1,4} = results.osc.K_lMPC.covradial{j}.n_g;
-    error{idx+4*j+1,5} = get_NRMSE(results.osc.K_lMPC.covradial{j}.NRMSE,1);
-    error{idx+4*j+1,6} = get_NRMSE(results.osc.K_lMPC.covradial{j}.NRMSE,2);
+    error{idx+4*j+1,5} = get_NRMSE(results.osc.K_lMPC.covradial{j}.RMSE,1);
+    error{idx+4*j+1,6} = get_NRMSE(results.osc.K_lMPC.covradial{j}.RMSE,2);
 end
 
 error = cell2table(error, "VariableNames", ...
-            ["Reference","Controller","Observable","Size","NRMSE_1","NRMSE_2"]);
+            ["Reference","Controller","Observable","Size","RMSE_1","RMSE_2"]);
 
 %% Identification set (skipped for now)
 
 %% Step reference (EKF)
 
-f = figure(2,"EKF (step)");
+f = figure("name","EKF (step)");
 
 % First state
 subplot(2,1,1);
@@ -109,7 +109,7 @@ exportgraphics(f, './step_reference_kalman.eps');
 
 %% Step reference (Koopman)
 
-f = figure(3,"Koopman (step)");
+f = figure("name","Koopman (step)");
 
 % First state
 subplot(2,1,1);
@@ -129,13 +129,13 @@ exportgraphics(f, './step_reference_koopman.eps');
 
 %% Oscillating reference (EKF)
 
-f = figure(4,"EKF (osc)");
+f = figure("name","EKF (osc)");
 
 % First state
 subplot(2,1,1);
 plot(t, results.osc.EKF_nlMPC.x_hat(1,:)); hold on; grid on;
 plot(t, results.osc.EKF_nlMPC.x(1,:));
-title("X_1"); xlim([t_start,t_limit]); ylim([-0.2 1.2]);
+title("X_1"); xlim([t_start,t_limit]); ylim([-1.2 1.2]);
 legend("Estimation","Real value",Location="southwest");
 
 % Second state
@@ -149,13 +149,13 @@ exportgraphics(f, './osc_reference_kalman.eps');
 
 %% Oscillating reference (Koopman)
 
-f = figure(5,"Koopman (osc)");
+f = figure("name","Koopman (osc)");
 
 % First state
 subplot(2,1,1);
 plot(t, results.osc.K_nlMPC.covradial{end}.x_hat(1,:)); hold on; grid on;
 plot(t, results.osc.K_nlMPC.covradial{end}.x(1,:));
-title("X_1"); xlim([t_start,t_limit]); ylim([-0.2 1.2]);
+title("X_1"); xlim([t_start,t_limit]); ylim([-1.2 1.2]);
 legend("Estimation","Real value",Location="southwest");
 
 % Second state
